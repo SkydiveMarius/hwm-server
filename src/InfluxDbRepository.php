cdb31ad7068aa0847e9b526c4d5ceba5391e76fc
Marius Meissner <info@marius-meissner.net>
2017-07-04 19:08:09 +0200

    Added influxdb repository

---
+++ src/InfluxDbRepository.php
@@ -0,0 +1,44 @@
+<?php
+namespace SkydiveMarius\HWM\Server\Src\CLI;
+
+use InfluxDB\Client;
+use InfluxDB\Database;
+use InfluxDB\Point;
+
+/**
+ * Class InfluxDbRepository
+ *
+ * @package SkydiveMarius\HWM\Server\Src\CLI
+ */
+class InfluxDbRepository
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var Database
+     */
+    private $database;
+
+    /**
+     * InfluxDbRepository constructor.
+     *
+     * @param Client      $client
+     * @param string|null $database
+     */
+    public function __construct(Client $client = null, string $database = null)
+    {
+        $this->client = $client ?: new Client(getenv('INFLUXDB_HOST'), getenv('INFLUXDB_PORT'), getenv('INFLUXDB_USERNAME'), getenv('INFLUXDB_PASSWORD'));
+        $this->database = $this->client->selectDB($database ?: getenv('INFLUXDB_DB'));
+    }
+
+    /**
+     * @param float $value
+     */
+    public function add(float $value)
+    {
+        $this->database->writePoints([new Point('distance', $value)]);
+    }
+}
